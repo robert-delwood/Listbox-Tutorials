d2526dadf8249ea91e34c333dd95d7e7111c944a
robert-delwood <87502826+robert-delwood@users.noreply.github.com>
2021-11-20 20:31:18 -0600

    Add files via upload

---
+++ Listbox1_Example_Project_1/Listbox1_Example/Listbox1_Example/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Listbox1_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(178, 91);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(619, 342);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Automatically add values";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "(No item selected)";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Add from textbox";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Value to add";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Clear listbox";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 134);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Delete selected item";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Listbox1_Example_Project_1/Listbox1_Example/Listbox1_Example/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Listbox1_Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddNumbericValues();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AddTextValues();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            label1.Text = "(No item selected)";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DeleteSelectedItem();
+        }
+
+        //Add a number of value to the list. Change length to add more or fewer items each time.
+        private void AddNumbericValues()
+        {
+            int length = 4;
+            for (int i = 0; i < length; i++)
+            {
+                listBox1.Items.Add(i);
+            }
+
+            //This is optional. If nothing is already selected, select the first item as a help to the user.
+            if (listBox1.SelectedIndex == -1) listBox1.SelectedIndex = 0;
+        }
+
+        //Add the value from textbox1 to the list.
+        private void AddTextValues()
+        {
+            string theString = textBox1.Text.Trim();
+            listBox1.Items.Add(theString);
+            //This is optional. If nothing is already selected, select the first item as a help to the user.
+            if (listBox1.SelectedIndex == -1) listBox1.SelectedIndex = 0;
+        }
+
+        private void DeleteSelectedItem()
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+
+            //If nothing is selected, return. There will be nothing to report. Added as a safety check.
+            if (selectedIndex == -1) return;
+
+            listBox1.Items.RemoveAt(selectedIndex);
+
+            //Reset the selected row to the same row as just deleted.
+            //There are three cases to account for.
+            //1) It's the last item in the listbox. In that case, nothing needs to be highlighted.
+            //2) It's the last item in the list of items. Then, highlight the one before it.
+            //3) it's not the last item in the list. Then, highlight the replacement item.
+            if (listBox1.Items.Count == 0) return;
+            if (selectedIndex >= listBox1.Items.Count)
+            {
+                listBox1.SelectedIndex = selectedIndex -1;
+                return;
+            }
+            if (listBox1.SelectedIndex == -1) listBox1.SelectedIndex = selectedIndex;
+        }
+
+        //This event occurs whenever the user makes a new selection in the list.
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+
+            //If nothing is selected, return. There will be nothing to report. Added as a safety check.
+            if (selectedIndex == -1) return;
+
+            var selectedValue = listBox1.SelectedItem;
+            label1.Text = string.Format("Selected index/value: {0}/{1}", selectedIndex, selectedValue);
+        }
+    }
+}

1cbb30a35f09887dab2651ef24d47f70e8927d6d
robert-delwood <87502826+robert-delwood@users.noreply.github.com>
2021-11-28 05:26:08 -0600

    Add files via upload

---
+++ Listbox3_Example_Project_3/Listbox3_Example_Project_3/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Listbox3_Example_Project_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(129, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(659, 384);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(16, 93);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(74, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Composite";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(16, 57);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Last name";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 23);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "First name";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(111, 127);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Show by...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Listbox3_Example_Project_3/Listbox3_Example_Project_3/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Listbox3_Example_Project_3
+{
+    public partial class Form1 : Form
+    {
+        List<Person> gPersonList = new List<Person>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create a database. A practical source would be a file or real time entry.
+            // See the Constructor method within the class Person.
+            gPersonList.Add(new Person("Andy", "Anselman", 29, 1));
+            gPersonList.Add(new Person("Boyd", "Braterschight", 45, 200));
+            gPersonList.Add(new Person("Charlie", "Chaleston", 17, 324));
+            gPersonList.Add(new Person("Danno", "Devenschultz", 56, 245));
+            gPersonList.Add(new Person("Everest", "Enselman", 62, 573));
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            listBox1.DataSource = null;
+            listBox1.DataSource = gPersonList;
+            listBox1.DisplayMember = "NameFirst";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton whichBox = (RadioButton)sender;
+
+            if (whichBox.Checked == true)
+            {
+                if (whichBox.Name == "radioButton1")
+                {
+                    listBox1.DisplayMember = "NameFirst";
+                }
+                else if (whichBox.Name == "radioButton2")
+                {
+                    listBox1.DisplayMember = "NameLast";
+                }
+                else
+                {
+                    listBox1.DisplayMember = "GetUserDetails";
+                }
+            }
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // Get the selected item, but you have to cast it to the correct class
+            var mySelectedItem = (Person)((ListBox) sender).SelectedItem;
+
+            if (mySelectedItem != null)
+            {
+                MessageBox.Show(string.Format("The person you selected is {0} {1},\r\nage {2}, and\r\nemployee number {3}.",
+                    mySelectedItem.NameFirst, mySelectedItem.NameLast, mySelectedItem.Age, mySelectedItem.EmployeeNumber),
+                    "Selected Employee", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
+
+// Class to be used with the ListBox
+public class Person
+{
+    // Properties
+    public string NameFirst { get; set; }
+    public string NameLast { get; set; }
+    public int Age { get; set; }
+    public int EmployeeNumber { get; set; }
+
+    // Constructor
+    public Person(string firstname, string lastname, int age, int employeeNumber)
+    {
+        NameFirst = firstname;
+        NameLast = lastname;
+        Age = age;
+        EmployeeNumber = employeeNumber;
+    }
+
+    // Methods
+    public string GetUserDetails
+    {
+        get{ return string.Format("Name: {0} {1}, Age: {2}", NameFirst, NameLast, Age); }
+    }
+}
+
